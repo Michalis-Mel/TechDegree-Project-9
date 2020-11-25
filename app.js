88ccb972b575b412b826a4c752430d5caa9eb259
Michalis Meliopoulos <mixalismeliop@gmail.com>
2020-11-25 14:24:24 +0200

    README and comments update

--- app.js
+++ app.js
@@ -40,8 +40,19 @@ app.use(express.json());
   }
 })();
 
+//Synchronizing the models with the database.
+(async () => {
 
-// TODO setup your api routes here
+  try {
+    await sequelize.sync()
+    console.log("Models successfully synchronize")
+
+  }catch(error) {
+    console.log("There was an error synchronizing the models to the data base")
+  }
+
+})();
+
 
 //Returns the currently authenticated user
 app.get('/api/users', authenticateUser, asyncHandler(async(req,res) => {
@@ -88,32 +99,97 @@ app.post('/api/users', asyncHandler(async(req,res) => {
       throw error
     }
   }
-  
 }))
 
 //Returns a list of courses (including the user that owns each course)
 app.get('/api/courses', asyncHandler(async(req,res) => {
-  
+    const courses = await Course.findAll({
+      attributes: ["id", "userId", "title", "description", "estimatedTime", "materialsNeeded"],
+      include: [{
+        attributes: ["id", "firstName", "lastName", "emailAddress"],
+        model:User
+      }]
+    });
+    res.json(courses);
 }))
 
 //Returns the course (including the user that owns the course) for the provided course ID
 app.get('/api/courses/:id', asyncHandler(async(req,res) => {
-  //200
+  const course = await Course.findAll({
+    where: {id: req.params.id},
+    attributes: ["id", "userId", "title", "description", "estimatedTime", "materialsNeeded"],
+    include: {
+      attributes: ["id", "firstName", "lastName", "emailAddress"],
+      model:User
+    }
+  });
+  res.json(course);
 }))
 
 //Creates a course, sets the Location header to the URI for the course, and returns no content
-app.post('/api/courses', asyncHandler(async(req,res) => {
-  //201
+app.post('/api/courses', authenticateUser, asyncHandler(async(req,res) => {
+  let course;
+  try{
+    course = await Course.create(req.body);
+    res.status(201).location('/api/courses/' + course.id).end()
+  }catch(error){
+    if(error.name === "SequelizeValidationError"){
+      res.status(400).json({"Error" : error.message})
+    }else{
+      throw error;
+    }
+  }
 }))
 
 //Updates a course and returns no content
-app.put('/api/courses/:id', asyncHandler(async(req,res) => {
-  //204
+//Using express-validator to make sure that the title and the description of the body are not empty
+app.put('/api/courses/:id', authenticateUser, [
+  body("title").notEmpty().withMessage("Please enter a title"),
+  body("description").notEmpty().withMessage("Please enter a description")
+ ], asyncHandler(async(req,res) => {
+  try{
+    
+    let course = await Course.findByPk(req.params.id);
+    if(course){
+      if(course.userId === req.currentUser.id){
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+          const errorArray = errors.array();
+          const message = errorArray.map(error => error.msg);
+          return res.status(400).json({ "Error" : message});
+        }else{
+          await course.update(req.body)
+          res.status(204).end()
+        }
+      }else{
+        res.status(403).json({ "Error" : "You are not authorized to edit this course"})
+      }
+    }else{
+      res.statu(404).end();
+    }
+  }catch(error){
+    if(error.name === "SequelizeValidationError") {
+      res.json({"Error":error.message})
+      } else {
+        throw error
+      }
+  }
+
 }))
 
 //Deletes a course and returns no content
-app.delete('/api/courses/:id', asyncHandler(async(req,res) => {
-  //204
+app.delete('/api/courses/:id', authenticateUser, asyncHandler(async(req,res) => {
+  let course = await Course.findByPk(req.params.id);
+  if(course){
+    if(course.userId === req.currentUser.id){
+      await course.destroy();
+      res.status(204).end();
+    }else{
+      res.status(403).json({ "Error" : "You are not authorized to delete this course"});
+    }
+  }else{
+    res.statu(404).end();
+  }
 }))
 
 
